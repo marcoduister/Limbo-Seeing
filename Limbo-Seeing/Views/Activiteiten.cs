f17dc131e263fe9dcb0c3a436afd229c77c0dd3c
marcoduister <marcoduister@hotmail.com>
2021-04-16 11:38:34 +0200

    rollen fix

--- Limbo-Seeing/Views/Activiteiten.cs
+++ Limbo-Seeing/Views/Activiteiten.cs
@@ -35,7 +35,7 @@ namespace Limbo_Seeing.Views
 
         private void Activiteiten_Load(object sender, EventArgs e)
         {
-            if (Properties.Settings.Default.UserRol != (int)Enums.Rolen.Beheerder || Properties.Settings.Default.UserRol != (int)Enums.Rolen.ActiviteitenManager)
+            if (Properties.Settings.Default.UserRol == (int)Enums.Rolen.Gebruiker)
             {
                 btn_beheer.Enabled = false;
                 btn_beheer.Visible = false;
